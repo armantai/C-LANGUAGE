a6a51dec7171c3f9fb322dca3b6247e6691792dc
armantai <taimunna722@gmail.com>
2025-09-09 12:32:24 +0530

    Add files via upload

--- project4/q6.c
+++ project4/q6.c
@@ -2,30 +2,41 @@
 
 int main(){
 
-    // for (int i=1; i<=5; i++)
-    // {
-    //    for (int j=1; j<=i; j++)
-    //    {
-    //     printf ("%d",j);
-    //    }
-    //    printf("\n");
-    // }
-
-
-
     for (int i=1; i<=5; i++)
     {
-        for (int k=5; k>i; k--)
+       for (int j=1; j<=i; j++)
+       {
+        printf ("%d",j);
+       }
+       for (int k=i; k<5; k++)
         {
             printf(" ");
         }
-        
-       for (int j=1; j<=5; j++)
+        for (int k=4; k>=i; k--)
+        {
+            printf(" ");
+        }
+
+         for (int j=i; j>=1; j--)
        {
         printf ("%d",j);
        }
        printf("\n");
     }
+
+
+
+    // for (int i=1; i<=5; i++)
+    // {
+        
+        
+    //    for (int j=1; j<=i; j++)
+    //    {
+    //     printf ("%d",j);
+    //    }
+       
+    //    printf("\n");
+    // }
     
 
 
